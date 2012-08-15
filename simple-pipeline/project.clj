97f9cc5889f5184d019449a77d6c911ed58daa0b
Debo~ Dutta <ddutta@gmail.com>
2012-08-14 23:29:48 -0700

    Fix some project.clj issues

--- simple-pipeline/project.clj
+++ simple-pipeline/project.clj
@@ -1,10 +1,14 @@
 (defproject simple-pipeline "1.0.0-SNAPSHOT"
   :java-source-path "src/java"
+  :source-path "src/clj"
   :javac-options {:debug "true" :fork "true"}
+  :aot :all
   :description "A simple pipeline for analytics using Storm"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [redis.clients/jedis "2.0.0"]]
-  :dev-dependencies [[storm "0.8.0"]]
+  :dependencies [[redis.clients/jedis "2.0.0"]
+                ]
+  :dev-dependencies [[storm "0.8.0"]
+                     [org.clojure/clojure "1.4.0"]
+                    ]
 )
 
 
